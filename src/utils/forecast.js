60f5fee1ef05a9e6475ed92c0dbd4959fad7da1e
SyamJalla <syamjalla@gmail.com>
2019-12-23 20:13:14 +0530

    Forecast detials added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary +'It is currently '+ body.currently.temperature+' degrees out. There is '+ body.currently.precipProbability+'% chance of rain.')
+            callback(undefined, body.daily.data[0].summary +'It is currently '+ body.currently.temperature+' degrees out. There is '+ body.currently.precipProbability+'% chance of rain with '+ body.currently.humidity + ' units as humidity and'+ body.currently.visibility +' units as visibility respectively')
         }
     })
 }
